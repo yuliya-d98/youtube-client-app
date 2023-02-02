bc45ca1d00935299ee615070c5df5530e278a109
Yuliya Dubovik <yuliya.dubovik@armtek.by>
2023-02-02 17:36:50 +0300

    start adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { SearchResultsComponent } from './seacrh/search-results/search-results.component';
+
 import { SearchItemComponent } from './seacrh/search-item/search-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { LogoComponent } from './header/logo/logo.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchResultsComponent,
-    SearchItemComponent
+    SearchItemComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/seacrh/search-item/search-item.component.ts
+++ src/app/seacrh/search-item/search-item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-search-item',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-item.component.scss']
 })
 export class SearchItemComponent {
+  viewModes = [
+    {title: 'Light', value: 'light', selected: true},
+    {title: 'Yellow', value: 'yellow', selected: false},
+    {title: 'Pink', value: 'pink', selected: false},
+    {title: 'Green', value: 'green', selected: false},
+    {title: 'Dark', value: 'dark', selected: false},
+  ]
+  selectedViewMode = this.viewModes[0].value;
+  isDarkMode: boolean = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
+  constructor() {}
+  selectViewMode($event: Event) {
+    console.log('$event.checked', ($event.currentTarget as HTMLSelectElement).value)
+  }
 }
